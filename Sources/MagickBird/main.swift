2d19a0463a21aa8bc58e7a9fcb6b2ea15826e425
Daniel <daniel@danramteke.com>
2019-03-22 11:26:23 -0400

    creating a test image

--- Sources/MagickBird/main.swift
+++ Sources/MagickBird/main.swift
@@ -1,3 +1,16 @@
 import MagickWandMac
 print("Hello, world!")
 // MagickWand.ConvertImageCommand()
+MagickWandGenesis()
+let wand = NewMagickWand()
+let pixel = NewPixelWand()
+
+PixelSetColor(pixel, "red")
+MagickSetBackgroundColor(wand, pixel)
+
+MagickNewImage(wand, 100, 100, pixel)
+MagickWriteImage(wand, "test.jpg")
+
+DestroyMagickWand(wand)
+DestroyPixelWand(pixel)
+MagickWandTerminus()
