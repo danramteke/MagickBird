5d435ff2840e8bf9850d4803fb11cbc46c7acb29
Daniel R <danramteke>
2021-06-09 02:06:21 -0400

    add width and height properties to Magick’s image

--- Sources/MagickBird/types/Image/Image-Size.swift
+++ Sources/MagickBird/types/Image/Image-Size.swift
@@ -7,4 +7,12 @@ extension Image {
     let height = Int(MagickGetImageHeight(self.pointer))
     return Size(width: width, height: height)
   }
+
+	public var width: Int {
+		Int(MagickGetImageWidth(self.pointer))
+	}
+
+	public var height: Int {
+		Int(MagickGetImageHeight(self.pointer))
+	}
 }
