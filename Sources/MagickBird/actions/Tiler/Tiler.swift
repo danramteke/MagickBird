5e7f502fb26876028c9f9110aa7db5899933083e
Daniel R <danramteke>
2021-06-09 20:38:01 -0400

    clean up tiler action

--- Sources/MagickBird/actions/Tiler/Tiler.swift
+++ Sources/MagickBird/actions/Tiler/Tiler.swift
@@ -1,59 +1,53 @@
 import MagickWand
 import Foundation
 
+public protocol TilerDelegate: AnyObject {
+	func didStart(level: Int)
+}
+
 public class Tiler {
-  public let tileSide = 512
+	public let tileSide: Int
 
   let originalImage: Image
   let maxLevel: Int
 	let filetype: String
   let writer: Writer
 
-	public init(inputPath: String, outputPath: String, outputFiletype: String = "png") {
+	weak var tilerDelegate: TilerDelegate?
+
+	public init(inputPath: String, outputPath: String, tileSide: Int = 512, outputFiletype: String = "png") {
     self.writer = Writer(outputDir: outputPath)
     self.originalImage = Image(filePath: inputPath)!
+		self.tileSide = tileSide
     self.maxLevel = originalImage.size.tilingTimes
     self.filetype = outputFiletype
   }
 
-  public init(input: Image, writer: Writer, outputFiletype: String = "png") {
+	public init(input: Image, writer: Writer, tileSide: Int = 512, outputFiletype: String = "png") {
     self.writer = writer
     self.originalImage = input
+		self.tileSide = tileSide
     self.maxLevel = input.size.tilingTimes
     self.filetype = outputFiletype
   }
 
   public func tile(levels onlyLevels: ClosedRange<Int>? = nil) {
-    print("Tiling...")
-    print(originalImage.size)
+		(0...maxLevel)
+			.reversed()
+			.filter { onlyLevels?.contains($0) ?? true }
+			.forEach { level in
 
-    let levels = (0...maxLevel).reversed()
-    levels.forEach { level in
-
-      if let onlyLevels = onlyLevels {
-        if onlyLevels.contains(level) {
-          makeTiles(for: originalImage, at: level) 
-        } else {
-          print("level \(level) -- SKIPPED")
-        }
-
-      } else {
-        makeTiles(for: originalImage, at: level) 
-      }
-
-      
-      originalImage.resize(by: 0.5)
+				makeTiles(for: originalImage, at: level)
+				originalImage.resize(by: 0.5)
     }
-
-    print("done Tiling")
   }
 
   private func makeTiles(for image: Image, at level: Int) {
-    print("level \(level)")
+		tilerDelegate?.didStart(level: level)
+
     let imageSize = image.size
 
     var position: Tile.Position = .zero 
-
 
     var x = 0
     while x < imageSize.width {
