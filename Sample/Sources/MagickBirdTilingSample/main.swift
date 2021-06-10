3434709c810f787a00043bff6e147bbf01e7860d
Daniel R <danramteke>
2021-06-09 21:44:43 -0400

    update sample

--- Sample/Sources/MagickBirdTilingSample/main.swift
+++ Sample/Sources/MagickBirdTilingSample/main.swift
@@ -3,14 +3,24 @@ import Foundation
 
 MagickBirdContext {
   Tiler(
-    inputPath: "./output/kitten.jpg", 
+    inputPath: "./output/india.png", 
     outputPath: "./output/jpg/", 
     outputFileExtension: "jpg") 
-    .tile(levels: (10...13))
+    .tile(levels: (6...10))
 
   Tiler(
-    inputPath: "./output/kitten.jpg", 
+    inputPath: "./output/india.png", 
     outputPath: "./output/png/", 
     outputFileExtension: "png")
-    .tile(levels: (10...13))
+    .tile(levels: (6...10))
+
+  Tiler(
+    inputPath: "./output/india.png", 
+    outputPath: "./output/png8/", 
+    outputFileExtension: "png") { tile in
+      let image = tile.image.clone()!
+      image.format = "PNG8"
+      return image
+    }
+    .tile(levels: (6...10))
 }
--- Sample/run-sample.sh
+++ Sample/run-sample.sh
@@ -2,6 +2,8 @@
 
 cd $(dirname $_) && \
     mkdir -p output/png && \
+    mkdir -p output/png8 && \
     mkdir -p output/jpg && \
-    curl -o output/kitten.jpg https://placekitten.com/5000/5000 && \
-    swift run && cd -
+    curl -o output/india.png https://upload.wikimedia.org/wikipedia/commons/7/77/Legality_of_cannabis_in_India.png && \
+    swift run && \
+    cd -
