30979dd337f471a3380aa706fd1d3c3cd6917869
Daniel R <danramteke>
2021-12-20 00:13:36 -0500

    provide a block for output filename

--- Sample/Sources/MagickBirdTilingSample/main.swift
+++ Sample/Sources/MagickBirdTilingSample/main.swift
@@ -5,19 +5,17 @@ MagickBirdContext {
   Tiler(
     inputPath: "./output/india.png", 
     outputPath: "./output/jpg/", 
-    outputFileExtension: "jpg") 
+    outputFilename: { zoom, x, y in "\(zoom)_\(x)_\(y).jpg"}) 
     .tile(levels: (6...10))
 
   Tiler(
     inputPath: "./output/india.png", 
-    outputPath: "./output/png/", 
-    outputFileExtension: "png")
+    outputPath: "./output/png/")
     .tile(levels: (6...10))
 
   Tiler(
     inputPath: "./output/india.png", 
-    outputPath: "./output/png8/", 
-    outputFileExtension: "png") { tile in
+    outputPath: "./output/png8/") { tile in
       let image = tile.image.clone()!
       image.format = "PNG8"
       return image
--- Sources/MagickBird/actions/Tiler/Tiler.swift
+++ Sources/MagickBird/actions/Tiler/Tiler.swift
@@ -6,26 +6,26 @@ public class Tiler {
   
   let originalImage: Image
   let maxLevel: Int
-  let fileExtension: String
   let writer: Writer
-  
+
+  let outputFilename: (Int, Int, Int) -> String
   let tileAdjustmentBlock: ((Tile) -> Image)?
   
-  public init(inputPath: String, outputPath: String, tileSide: Int = 512, outputFileExtension: String = "png", tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
+  public init(inputPath: String, outputPath: String, tileSide: Int = 512, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
     self.writer = Writer(outputDir: outputPath)
     self.originalImage = Image(filePath: inputPath)!
     self.tileSide = tileSide
     self.maxLevel = originalImage.size.tilingTimes
-    self.fileExtension = outputFileExtension
+    self.outputFilename = outputFilename
     self.tileAdjustmentBlock = tileAdjustmentBlock
   }
   
-  public init(input: Image, writer: Writer, tileSide: Int = 512, outputFileExtension: String = "png", tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
+  public init(input: Image, writer: Writer, tileSide: Int = 512, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
     self.writer = writer
     self.originalImage = input
     self.tileSide = tileSide
     self.maxLevel = input.size.tilingTimes
-    self.fileExtension = outputFileExtension
+    self.outputFilename = outputFilename
     self.tileAdjustmentBlock = tileAdjustmentBlock
   }
   
@@ -57,7 +57,8 @@ public class Tiler {
         
         let tile = Tile(level: level, position: position, image: tileImage)
         let adjustedTileImage = tileAdjustmentBlock?(tile) ?? tile.image
-        try! writer.write(image: adjustedTileImage, to: "\(tile.filestem).\(fileExtension)")
+        let filename: String = outputFilename(tile.level, tile.position.x, tile.position.y)
+        try! writer.write(image: adjustedTileImage, to: filename)
         
         point = point.incrementingY(by: tileSide)
         position = position.incrementingY
