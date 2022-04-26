3be949e92bcee03e19a7079c91da416825c3b91a
Your Name <daniel@danramteke.com>
2022-04-26 11:41:24 -0400

    announce

--- Sources/MagickBird/actions/Tiler/Tiler.swift
+++ Sources/MagickBird/actions/Tiler/Tiler.swift
@@ -7,26 +7,29 @@ public class Tiler {
   let originalImage: Image
   let maxLevel: Int
   let writer: Writer
+  let isQuiet: Bool
 
   let outputFilename: (Int, Int, Int) -> String
   let tileAdjustmentBlock: ((Tile) -> Image)?
   
-  public init(inputPath: String, outputPath: String, tileSide: Int = 512, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
+  public init(inputPath: String, outputPath: String, tileSide: Int = 512, isQuiet: Bool = true, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
     self.writer = Writer(outputDir: outputPath)
     self.originalImage = Image(filePath: inputPath)!
     self.tileSide = tileSide
     self.maxLevel = originalImage.size.tilingTimes
     self.outputFilename = outputFilename
     self.tileAdjustmentBlock = tileAdjustmentBlock
+    self.isQuiet = isQuiet
   }
   
-  public init(input: Image, writer: Writer, tileSide: Int = 512, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
+  public init(input: Image, writer: Writer, tileSide: Int = 512, isQuiet: Bool = true, outputFilename: @escaping (Int, Int, Int) -> String = { zoom, x, y in "\(zoom)_\(x)_\(y).png" }, tileAdjustmentBlock: ( (Tile) -> Image)? = nil) {
     self.writer = writer
     self.originalImage = input
     self.tileSide = tileSide
     self.maxLevel = input.size.tilingTimes
     self.outputFilename = outputFilename
     self.tileAdjustmentBlock = tileAdjustmentBlock
+    self.isQuiet = isQuiet
   }
   
   public func tile(levels onlyLevels: ClosedRange<Int>? = nil) {
@@ -34,6 +37,7 @@ public class Tiler {
       .reversed()
       .filter { onlyLevels?.contains($0) ?? true }
       .forEach { level in
+        if !isQuiet { print("level \(level)")}
         
         makeTiles(for: originalImage, at: level)
         originalImage.resize(by: 0.5)
